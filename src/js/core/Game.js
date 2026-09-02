285c416eb3da9a79f4b4a29e4ec5896816760086
meatbags <jxburrow@gmail.com>
2026-09-02 16:31:35 +1000

    update

--- src/js/core/Game.js
+++ src/js/core/Game.js
@@ -49,10 +49,10 @@ class Game extends SceneNode {
     this.add( new Overworld() );
 
     // rooms
-    //this.add( new Room_01() );
+    this.add( new Room_01() );
     //this.add( new Room_02() );
     //this.add( new Room_03() );
-    this.add( new Room_04() );
+    //this.add( new Room_04() );
     //this.add( new Room_05() );
     //this.add( new Room_06() );
     //this.add( new Room_07() );
@@ -64,7 +64,7 @@ class Game extends SceneNode {
     this.add( new Room_12() );
     this.add( new Room_13() );
     */
-
+    
     // optimisation
     //this.add( new Frustum() );
   }
